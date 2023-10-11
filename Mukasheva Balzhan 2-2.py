ecfb7c5f621325208a07788d3f765333b157bed3
BalzhanMukasheva <147227541+BalzhanMukasheva@users.noreply.github.com>
2023-10-11 09:04:05 +0600

    Update Mukasheva Balzhan 2-2.py

--- Mukasheva Balzhan 2-2.py
+++ Mukasheva Balzhan 2-2.py
@@ -1,18 +1,16 @@
-#код в котором может возникнуть исключение помещено в try
 try:
-    #вводим четырехзначное число
-    num=input("Enter a four-digit number:")
-    #выводим на экран позиции четырехзначного числа с помощью индексов
-    thousands = number // 1000
-    hundreds = (number // 100) % 10
-    tens = (number // 10) % 10
-    ones = number % 10
+        # Запрос на ввод четырехзначного числа
+        number = int(input("Введите четырехзначное число: "))
+        thousands = number // 1000
+        hundreds = (number // 100) % 10
+        tens = (number // 10) % 10
+        ones = number % 10
 
-    print("Цифры числа:")
-    print("Тысячи:", thousands)
-    print("Сотни:", hundreds)
-    print("Десятки:", tens)
-    print("Единицы:", ones)
+        print("Цифры числа:")
+        print("Тысячи:", thousands)
+        print("Сотни:", hundreds)
+        print("Десятки:", tens)
+        print("Единицы:", ones)
 
-except ValueError:
-    print("Ввод данных неверный")
+except ValueError as e:
+        print("Ошибка:", e)
