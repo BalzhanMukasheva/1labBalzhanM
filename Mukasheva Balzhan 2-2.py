26dc4b262739cdbf75459bc143581857cbd286b3
BalzhanMukasheva <147227541+BalzhanMukasheva@users.noreply.github.com>
2023-10-11 08:52:07 +0600

    Update Mukasheva Balzhan 2-2.py

--- Mukasheva Balzhan 2-2.py
+++ Mukasheva Balzhan 2-2.py
@@ -3,9 +3,16 @@ try:
     #вводим четырехзначное число
     num=input("Enter a four-digit number:")
     #выводим на экран позиции четырехзначного числа с помощью индексов
-   first_position=num/10
-   second_position=num%10
-   print(f"Позиция первая {first})
+    thousands = number // 1000
+    hundreds = (number // 100) % 10
+    tens = (number // 10) % 10
+    ones = number % 10
+
+    print("Цифры числа:")
+    print("Тысячи:", thousands)
+    print("Сотни:", hundreds)
+    print("Десятки:", tens)
+    print("Единицы:", ones)
 
 except ValueError:
     print("Ввод данных неверный")
